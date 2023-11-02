ef03b61416f30f1a0dd3839e36a4651b9f935c69
AarthiSelvaraj2611 <149648071+AarthiSelvaraj2611@users.noreply.github.com>
2023-11-02 11:17:11 +0530

    Create datasetupload.py

---
+++ datasetupload.py
@@ -0,0 +1,2 @@
+import pandas as py
+dataset = py.read_excel("F:\SREC\mypapaerwork\Predictive Analytics Forecasting Patient Outcomes and Medical Trends\dataset\Prediction of Sepsis - PhysioNet Computing in Cardiology Challenge 2019\Dataset.csv")
